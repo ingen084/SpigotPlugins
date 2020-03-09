67fc294fcba76853714783a74faba3edfaee2458
ingen084 <ingen188@gmail.com>
2020-03-10 03:39:48 +0900

    Minecraft 1.15.2 Java11対応

--- ingenCustomCartSpeed/src/main/java/net/ingen084/bukkit/ingencustomcartspeed/CustomCartSpeed.java
+++ ingenCustomCartSpeed/src/main/java/net/ingen084/bukkit/ingencustomcartspeed/CustomCartSpeed.java
@@ -2,11 +2,8 @@ package net.ingen084.bukkit.ingencustomcartspeed;
 
 import java.text.MessageFormat;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -29,20 +26,20 @@ public class CustomCartSpeed implements Listener {
         if(!(event.getVehicle() instanceof Minecart))
             return;
 
-        double speed = 28.8D;
-        Location toLocation = event.getTo();
-        Block railBlock = toLocation.getBlock();
+        var speed = 28.8D;
+        var toLocation = event.getTo();
+        var railBlock = toLocation.getBlock();
 
         // 座標･レールチェック
-        if(toLocation.getY() < 2 || railBlock.getType() != Material.DETECTOR_RAIL)
+        if (toLocation.getY() < 2 || railBlock.getType() != Material.DETECTOR_RAIL)
             return;
         // 看板チェック
-        Block signBlock = railBlock.getRelative(0, -2, 0);
-        if (!signBlock.getType().equals(Material.WALL_SIGN) && !signBlock.getType().equals(Material.SIGN))
+        var signBlock = railBlock.getRelative(0, -2, 0);
+        if (!signBlock.getType().equals(Material.OAK_WALL_SIGN) && !signBlock.getType().equals(Material.OAK_SIGN))
             return;
         
         // 看板から読み取り
-        String[] lines = ((Sign)signBlock.getState()).getLines();
+        var lines = ((Sign)signBlock.getState()).getLines();
         if (!lines[0].equalsIgnoreCase("[cart speed]"))
             return;
         
@@ -57,18 +54,18 @@ public class CustomCartSpeed implements Listener {
         speed /= 72;
         if (speed == ((Minecart) event.getVehicle()).getMaxSpeed())
             return;
-        ((Minecart)event.getVehicle()).setMaxSpeed(speed);
+        ((Minecart) event.getVehicle()).setMaxSpeed(speed);
 
         // メッセージ生成
-        ChatColor color = ChatColor.GOLD;
-        double kmhSpeed = speed * 72;
-        if(kmhSpeed >= 107.9999)
+        var color = ChatColor.GOLD;
+        var kmhSpeed = speed * 72;
+        if (kmhSpeed >= 107.9999)
             color = ChatColor.RED;
-        else if((int)kmhSpeed >= 99.9999)
+        else if ((int)kmhSpeed >= 99.9999)
             color = ChatColor.GOLD;
 
         // 搭乗者全員にメッセージを送信
-        for (Entity entity : event.getVehicle().getPassengers())
+        for (var entity : event.getVehicle().getPassengers())
             if (entity instanceof Player)
                 ((Player)entity).sendTitle("",ChatColor.BLUE + "MAX SPEED: " + color + MessageFormat.format("{0,number,000.000} km/h", kmhSpeed), 5, 10, 5);
     }
